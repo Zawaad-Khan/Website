57ad3798a6d77b8170da3e0c8256f1014e2d4813
Zawaad Khan <zawaadkhan6@gmail.com>
2025-06-26 18:13:27 +0600

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,3 +2,10 @@ function toggleMenu() {
   const menu = document.getElementById('side-menu');
   menu.classList.toggle('hidden');
 }
+document.addEventListener('click', function(event) {
+  const menu = document.getElementById('side-menu');
+  const hamburger = document.querySelector('.hamburger');
+  if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
+    menu.classList.add('hidden');
+  }
+});
