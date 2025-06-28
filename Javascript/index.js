ac5d44461327c437b31b5e116dfefd90becb5dce
Zawaad Khan <zawaadkhan6@gmail.com>
2025-06-28 13:42:00 +0600

    3333

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,11 +1,12 @@
 function toggleMenu() {
   const menu = document.getElementById('side-menu');
-  menu.classList.toggle('hidden');
+  menu.classList.toggle('show');
 }
+
 document.addEventListener('click', function(event) {
   const menu = document.getElementById('side-menu');
   const hamburger = document.querySelector('.hamburger');
   if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
-    menu.classList.add('hidden');
+    menu.classList.remove('show');
   }
 });
